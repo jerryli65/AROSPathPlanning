f62a01adae64ab2e953c334309543737098dc6c7
Samuel He <samuel.he@tamu.edu>
2020-06-01 16:58:55 -0500

    Created basic functions to move robot a certain distance, as well as create paths for the robot to follow. IN PROGRESS: Creating a function to output motor values in order for the drive chassis to follow an instantiated path. still working on commenting all the code.
    Signed-off-by: Samuel He <samuel.he@tamu.edu>

--- src/Odometry.cpp
+++ src/Odometry.cpp
@@ -1,30 +1,99 @@
 #include <iostream>
+#include <iostream>
 #include <cmath>
+#include "odometry.hpp"
+#include <vector>
 
-float encoderMax = 360;
-float xPos;
-float yPos;
-float head;
+using namespace std;
 
-float distance(float value, float radius){ // takes in encoder ticks for "value" and radius of wheel
-    return (float) (2 * (value / encoderMax) * M_PI * radius); // rotations
-}
+struct position{ // class for tracking position
+    float x, y, h;
+    void reset(){
+        x = 0;
+        y = 0;
+        h = 0;
+    }
+};
 
-float heading(float hPrev, float left, float right, float b, float radius){
-    float theta = (distance(left, radius) -
-                   distance(right, radius)) / (b); // calculation for heading based on left and right wheel encoders
-    float newHead = hPrev + theta;
-    return newHead;
-}
+struct wayPoint{ // class for inputting waypoints
+    float x, y;
+};
 
-float positionChange(float prevPos, float left, float right, float radius){
-    float newPos = (distance(left, radius) +
-                    distance(right, radius)) / 2;
-    return prevPos + newPos;
-}
+class path{ // class for creating a path for the robot to follow
+public:
+    vector<wayPoint> path;
+    void add(float x, float y){
+        path.push_back(wayPoint{x, y});
+    }
+    void remove(int index){
+        path.erase(path.begin() + index);
+    }
+    void clear(){
+        path.erase(path.begin() + path.size());
+    }
+};
 
-void initPos(){
-    xPos = 0;
-    yPos = 0;
-    head = 0;
-}
+class chassis{ // class for initializing the robot chassis as a variable
+    float lPrevError, rPrevError, rD, lD, lIntegral, rIntegral, kP, kI, kD;
+public:
+    position p;
+    float width/*wheel to wheel chassis width*/,
+            radius /*wheel radius*/,
+            encoderMax/*encoder ticks for 1 rotation*/,
+            threshold/*integral threshold for adding*/; //constants unique to each drive chassis
+    bool complete;
+
+    //sets up k constants for PID control
+    void setPID_const(float P, float I, float D){
+        kP = P;
+        kI = I;
+        kD = D;
+    }
+
+    void resetIntegrals(){
+        lIntegral = 0;
+        rIntegral = 0;
+    }
+
+    /*to use:
+     * while(!chassisName.complete){
+     *      chassisName.move =
+     * }*/
+
+    auto move(/*simple PID controller*/
+            float distance/*target distance to reach*/,
+            float left/*encoder values of the left chassis*/,
+            float right/*encoder values of right chassis*/,
+            float waitTime/*waitTime between each cycle of the while loop*/
+    ){
+        struct motorPower{
+            float left, right;
+        };
+        float lError = distance - left;
+        float rError = distance - right;
+        if(lError == 0 && rError == 0){
+            return motorPower{0, 0};
+        }
+        if(abs(distance - left) >= threshold){
+            lIntegral = 0;
+        } else{
+            lIntegral++;
+        }
+
+        if(abs(distance - right) >= threshold){
+            rIntegral = 0;
+        } else{
+            rIntegral++;
+        }
+
+        lD = kD * (lError - lPrevError) / waitTime;
+        rD = kD * (rError - rPrevError) / waitTime;
+
+        float leftPower = (lError) * kP + lIntegral * kI + lD * kD; //power allocated to left motors
+        float rightPower = (rError) * kP + rIntegral * kI + rD * kD; // power allocated to right
+
+        lPrevError = lError;
+        rPrevError = rError;
+        return motorPower{leftPower, rightPower};
+    }
+};
