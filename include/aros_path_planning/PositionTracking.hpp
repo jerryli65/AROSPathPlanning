8076a2194b327fb0d103490fcd3a22df17461d06
Brett Etter <bbetter9101@gmail.com>
2020-06-08 21:52:18 -0500

    Fixed some math and updated googletest

--- include/aros_path_planning/PositionTracking.hpp
+++ include/aros_path_planning/PositionTracking.hpp
@@ -11,26 +11,38 @@ using namespace aros::ChassisInit;
 
 namespace aros::PositionTracking{
     typedef int64_t EncoderType;
+
     struct Position{
         /**
          * x: x position
          * y: y position
          * h: heading (or angle in relative to starting position)
          */
-        float x, y, h;
+        float x, y, angle;
     };
 
-    class _Position{
-        Position p;
-        ChassisDefinition c;
-        EncoderType prevL, prevR, prevB;
+    struct ResetState{
+        Position position;
+        EncoderType right_encoder;
+        EncoderType left_encoder;
+        EncoderType back_encoder;
+    };
+
+    class PositionTracker{
+        Position _last_position;
+        const ChassisDefinition _chassis_definition;
+        EncoderType _last_left_position, _last_right_position, _last_back_position;
+        ResetState _reset_state;
     public:
+        PositionTracker(const ResetState& reset_state, const ChassisDefinition& chassis_definition);
         /**
          *
-         * @param right : right tracking wheel encoder values
-         * @param left : left tracking wheel encoder values
+         * @param right right tracking wheel encoder values
+         * @param left left tracking wheel encoder values
+         * @param back back tracking wheel encoder values
          * @return returns the motor power values of the left and right chassis
          */
-        auto Track(EncoderType right, EncoderType left, EncoderType back);
+        auto track(EncoderType right, EncoderType left, EncoderType back) -> Position;
+        auto reset(const ResetState& reset_state);
     };
 }
--- src/PositionTracking.cpp
+++ src/PositionTracking.cpp
@@ -1,22 +1,73 @@
 #include "aros_path_planning/PositionTracking.hpp"
 
 namespace aros::PositionTracking{
-    auto _Position::Track(EncoderType right, EncoderType left, EncoderType back){
-        float dx;
-        float dy;
-        float distanceLeft = ((left - prevL) / c.ticksPerRev()) * M_PI * c.diameter();
-        float distanceRight = ((right - prevR) / c.ticksPerRev()) * M_PI * c.diameter();
-        float distanceBack = ((back - prevB) / c.ticksPerRev()) * M_PI * c.diameter();
-        float dTheta = (distanceLeft - distanceRight) / (c.Left() + c.Right());
-        if(dTheta == 0){
-            dx = distanceBack * cos(p.h) + distanceRight * sin(p.h); ///This works
-            dy = distanceRight * cos(p.h) + distanceBack * sin(p.h);
+    template<typename T>
+    inline auto CartesianToPolar(T x, T y, T& r_out, T& theta_out){
+        r_out = sqrt(x * x + y * y);
+        theta_out = atan(y / x);
+    }
+
+    template<typename T>
+    inline auto PolarToCartesian(T r, T theta, T& x_out, T& y_out){
+        x_out = r * cos(theta);
+        y_out = r * sin(theta);
+    }
+
+    template<typename T>
+    inline auto Rotate(T x, T y, T rotate_theta, T& x_out, T& y_out){
+        float r, theta;
+        CartesianToPolar(x, y, r, theta);
+        theta += rotate_theta;
+        PolarToCartesian(r, theta, x_out, y_out);
+    }
+
+    auto PositionTracker::track(EncoderType right, EncoderType left, EncoderType back) -> Position{
+        float delta_left = ((left - _last_left_position) / _chassis_definition.ticksPerRev()) * M_PI * _chassis_definition.diameter();
+        float delta_right = ((right - _last_right_position) / _chassis_definition.ticksPerRev()) * M_PI * _chassis_definition.diameter();
+        float delta_back = ((back - _last_back_position) / _chassis_definition.ticksPerRev()) * M_PI * _chassis_definition.diameter();
+
+        float delta_left_absolute = ((left - _reset_state.left_encoder) / _chassis_definition.ticksPerRev()) * M_PI * _chassis_definition.diameter();
+        float delta_right_absolute = ((right - _reset_state.right_encoder) / _chassis_definition.ticksPerRev()) * M_PI * _chassis_definition.diameter();
+
+        float theta_current = _reset_state.position.angle + (delta_left_absolute - delta_right_absolute) / (_chassis_definition.Left() + _chassis_definition.Right());
+        float delta_theta = theta_current - _last_position.angle;
+
+        float delta_x_local;
+        float delta_y_local;
+        if(delta_theta == 0){
+            delta_x_local = delta_back; ///This works
+            delta_y_local = (delta_right + delta_left) / 2;
         } else{
-            dx = (2 * sin(M_PI / 2) * ((distanceBack / dTheta) + c.Back())) * cos(p.h) + (2 * sin(M_PI / 2) * ((distanceRight / dTheta) + c.Right())) * sin(p.h); ///Idk if this does though
-            dy = (2 * sin(M_PI / 2) * ((distanceRight / dTheta) + c.Right())) * cos(p.h) + (2 * sin(M_PI / 2) * ((distanceBack / dTheta) + c.Back())) * sin(p.h);
+            delta_x_local = 2 * sin(delta_theta) * (delta_back / delta_theta + _chassis_definition.Back());
+            float y_r = 2 * sin(delta_theta) * (delta_right / delta_theta + _chassis_definition.Right());
+            float y_l = 2 * sin(delta_theta) * (delta_left / delta_theta - _chassis_definition.Left());
+            delta_y_local = (y_r + y_l) / 2;
         }
-        p.y += dy;
-        p.x += dx;
-        p.h += dTheta;
-    };
+
+        float theta_average = _reset_state.position.angle + delta_theta / 2;
+
+        float delta_x, delta_y;
+        Rotate(delta_x_local, delta_y_local, -theta_average, delta_x, delta_y);
+
+        _last_position.x += delta_x;
+        _last_position.y += delta_y;
+        _last_position.angle = theta_current;
+
+        _last_left_position = left;
+        _last_right_position = right;
+        _last_back_position = back;
+
+        return Position{_last_position};
+    }
+
+    PositionTracker::PositionTracker(const ResetState& reset_state, const ChassisDefinition& chassis_definition) : _reset_state(reset_state), _last_position(reset_state.position), _chassis_definition(chassis_definition),
+                                                                                                                   _last_left_position(reset_state.left_encoder), _last_right_position(reset_state.right_encoder),
+                                                                                                                   _last_back_position(reset_state.back_encoder){}
+    auto PositionTracker::reset(const ResetState& reset_state){
+        _reset_state = reset_state;
+        _last_position = reset_state.position;
+        _last_left_position = reset_state.left_encoder;
+        _last_right_position = reset_state.right_encoder;
+        _last_back_position = reset_state.back_encoder;
+    }
 }
