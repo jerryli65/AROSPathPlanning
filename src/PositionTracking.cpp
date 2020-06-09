1f19c95cec660b223c316509470286db54111d6e
Samuel He <samuel.he@tamu.edu>
2020-06-08 19:40:35 -0500

    done with position tracking
    Signed-off-by: Samuel He <samuel.he@tamu.edu>

--- src/PositionTracking.cpp
+++ src/PositionTracking.cpp
@@ -2,15 +2,21 @@
 
 namespace aros::PositionTracking{
     auto _Position::Track(EncoderType right, EncoderType left, EncoderType back){
-
+        float dx;
+        float dy;
         float dl = 2 * ((left - prevL) / c.encoderMax()) * M_PI * c.radius();
         float dr = 2 * ((right - prevR) / c.encoderMax()) * M_PI * c.radius();
         float db = 2 * ((back - prevB) / c.encoderMax()) * M_PI * c.radius();
         float dTheta = (dl - dr) / (c.sL() + c.sR());
 
 
-        float dx = 2 * sin(M_PI / 2) * ((db / dTheta) + c.sB());
-        float dy = 2 * sin(M_PI / 2) * ((dr / dTheta) + c.sR());
+        if(dTheta == 0){
+            dx = db;
+            dy = dr;
+        } else{
+            dx = 2 * sin(M_PI / 2) * ((db / dTheta) + c.sB());
+            dy = 2 * sin(M_PI / 2) * ((dr / dTheta) + c.sR());
+        }
 
         p.y += dy;
         p.x += dx;
