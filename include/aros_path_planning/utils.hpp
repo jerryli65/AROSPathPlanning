7a3346ffc4a5ada77b142ea7d25c3b856d5044ef
Samuel He <samuel.he@tamu.edu>
2020-06-10 17:48:40 -0500

    moved CartesiantoPolar and PolartoCartesian to a utils file
    Signed-off-by: Samuel He <samuel.he@tamu.edu>

--- include/aros_path_planning/utils.hpp
+++ include/aros_path_planning/utils.hpp
@@ -1,16 +1,33 @@
-//
-// Created by david on 6/10/2020.
-//
 #include <cmath>
-
 #ifndef AROSPATHPLANNING_UTILS_HPP
 #define AROSPATHPLANNING_UTILS_HPP
 
 #endif //AROSPATHPLANNING_UTILS_HPP
 namespace aros::utils{
+    /** Converts a Cartesian coordinate to a polar coordinate
+    *
+    * @param x input cartesian x-coordinate
+    * @param y input cartesian y-coordinate
+    * @param r_out output polar r-coordinate
+    * @param theta_out output polar theta coordinate
+    * @return a polar coordinate, given an input Cartesian coordinate
+    */
     template<typename T>
-    inline auto CartesianToPolar(T x, T y, T& r_out, T& theta_out);
-
+    inline auto CartesianToPolar(T x, T y, T& r_out, T& theta_out) -> void{
+        r_out = sqrt(x * x + y * y);
+        theta_out = atan(y / x);
+    }
+    /** Converts a polar coordinate to a cartesian coordinate
+     *
+     * @param r input Polar r-coordinate
+     * @param theta input Polar theta coordinate
+     * @param x_out output Cartesian x-coordinate
+     * @param y_out output Cartesian y-coordinate
+     * @return a Cartesian coordinate, given an input Polar coordinate
+     */
     template<typename T>
-    inline auto PolarToCartesian(T r, T theta, T& x_out, T& y_out);
+    inline auto PolarToCartesian(T r, T theta, T& x_out, T& y_out) -> void{
+        x_out = r * cos(theta);
+        y_out = r * sin(theta);
+    }
 }
--- src/PositionTracking.cpp
+++ src/PositionTracking.cpp
@@ -1,33 +1,6 @@
 #include "aros_path_planning/PositionTracking.hpp"
 
-using namespace aros::utils;
 namespace aros::PositionTracking{ // namespace containing functions to track robot position
-    /** Converts a Cartesian coordinate to a polar coordinate
-    *
-    * @param x input cartesian x-coordinate
-    * @param y input cartesian y-coordinate
-    * @param r_out output polar r-coordinate
-    * @param theta_out output polar theta coordinate
-    * @return a polar coordinate, given an input Cartesian coordinate
-    */
-    template<typename T>
-    inline auto CartesianToPolar(T x, T y, T& r_out, T& theta_out){
-        r_out = sqrt(x * x + y * y);
-        theta_out = atan(y / x);
-    }
-    /** Converts a polar coordinate to a cartesian coordinate
-     *
-     * @param r input Polar r-coordinate
-     * @param theta input Polar theta coordinate
-     * @param x_out output Cartesian x-coordinate
-     * @param y_out output Cartesian y-coordinate
-     * @return a Cartesian coordinate, given an input Polar coordinate
-     */
-    template<typename T>
-    inline auto PolarToCartesian(T r, T theta, T& x_out, T& y_out){
-        x_out = r * cos(theta);
-        y_out = r * sin(theta);
-    }
     /**
     *
     * @param x input previous global y-coordinate
@@ -39,7 +12,7 @@ namespace aros::PositionTracking{ // namespace containing functions to track rob
     */
     template<typename T>
     inline auto Rotate(T x, T y, T rotate_theta, T& x_out, T& y_out){
-        float r, theta;
+        T r, theta;
         CartesianToPolar(x, y, r, theta);
         theta += rotate_theta;
         PolarToCartesian(r, theta, x_out, y_out);
